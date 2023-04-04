76334cdd320ed1baf1bad0942101d9ad3b06c4bc
rcsang <sangmath@outlook.com>
2023-04-04 23:32:53 +0200

    commit

--- popup.js
+++ popup.js
@@ -1,15 +1,3 @@
-// A2Z F17
-// Daniel Shiffman
-// http://shiffman.net/a2z
-// https://github.com/shiffman/A2Z-F17
-
-// This code runs when the pop up opens
-
-// Wordnik API
-var wordnik1 = 'http://api.wordnik.com:80/v4/word.json/'
-var wordnik2 = '/definitions?limit=1&includeRelated=true&useCanonical=false&includeTags=false';
-var api_key  = '&api_key=48dd829661f515d5abc0d03197a00582e888cc7da2484d5c7';
-
 async function setup() {
   noCanvas();
   // This is a way of getting a global variable from the background script
@@ -20,7 +8,7 @@ async function setup() {
   const endpoint = 'https://api.openai.com/v1/completions';
 
   // Query the OpenAI API
-  const prompt = `Explain the following text in simple terms: "${word}"`;
+  const prompt = `Assume that I'm a beginner and explain the given text in a detailed way using simple words: "${word}"`;
   console.log('asking for fetch requestion');
   const response = await fetch(endpoint, {
     method: 'POST',
@@ -31,7 +19,7 @@ async function setup() {
     body: JSON.stringify({
       "model": "text-davinci-003",
       "prompt": prompt,
-      "max_tokens": 100,
+      "max_tokens": 256,
       "n": 1,
       "stop": null,
       "temperature": 1,
